c58500c29bc80faca7be770858dd29b5b3f0c982
Sam Hadfield <sthtech@protonmail.com>
2024-12-31 11:38:16 -0600

    Construct resetSquares function, alter variable
    Build out 'resetSquares' helper function to enable the ability to
    reset the grid. Change 'numberOfSquares' variable name to 'squares' in
    order to lessen confusion with 'grid' function parameter. Test 'button'
    function and confirm that it is working properly.

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
+// Establishing number of rows and columns
 const container = document.querySelector(".container");
-let numberOfSquares = 16;
+let squares = 16;
+grid(squares);
 
-// Establishing default columns and rows
-for (let i = 0; i < numberOfSquares; i++) {
-  const row = document.createElement("div");
-  row.classList.add("row");
-
+// Creating grid of rows and columns
+function grid(numberOfSquares) {
   for (let i = 0; i < numberOfSquares; i++) {
-    const column = document.createElement("div");
-    column.classList.add("column");
-    column.addEventListener("mouseover", () => {
-      column.style.backgroundColor = "#179BAE";
-    });
-    row.appendChild(column);
+    const row = document.createElement("div");
+    row.classList.add("row");
+
+    for (let i = 0; i < numberOfSquares; i++) {
+      const column = document.createElement("div");
+      column.classList.add("column");
+      column.addEventListener("mouseover", () => {
+        column.style.backgroundColor = "#179BAE";
+      });
+      row.appendChild(column);
+    }
+    container.appendChild(row);
   }
-  container.appendChild(row);
 }
 
 // Add "reset" button with prompt for user input
@@ -23,6 +27,13 @@ document.body.insertBefore(button, container);
 button.id = "reset-button";
 button.addEventListener("click", resetSquares);
 
-// Haven't committed anything below
 // Construct resetSquares() callback function
-function resetSquares() {}
+function resetSquares() {
+  container.innerHTML = "";
+  let squares = prompt("How many squares should there be on each side?");
+  if (squares >= 1 && squares <= 100) {
+    grid(squares);
+  } else {
+    alert("You entered an incorrect value.");
+  }
+}
